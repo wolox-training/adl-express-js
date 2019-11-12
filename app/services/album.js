2096ef4beb4f39cbac22ad070c5fbfea15a14de6
adlamas <alan12394@hotmail.com>
2019-11-12 13:43:22 -0300

    changed travis.yml, refactored services to add catch method, added external api error in errors.js

--- app/services/album.js
+++ app/services/album.js
@@ -1,6 +1,14 @@
 const axios = require('axios');
+const logger = require('../logger');
+const errors = require('../errors');
 
 exports.albums = () =>
-  axios.get('https://jsonplaceholder.typicode.com/albums').then(response => {
-    console.log(response);
-  });
+  axios
+    .get('https://jsonplaceholder.typicode.com/albums')
+    .then(response => {
+      logger.info(`Received albums: ${JSON.stringify(response)}`);
+      return response;
+    })
+    .catch(() => {
+      throw errors.external_api_error('Error in external API');
+    });
--- app/services/photo.js
+++ app/services/photo.js
@@ -1,6 +1,14 @@
 const axios = require('axios');
+const logger = require('../logger');
+const errors = require('../errors');
 
 exports.photos = () =>
-  axios.get('https://jsonplaceholder.typicode.com/photos').then(response => {
-    console.log(response);
-  });
+  axios
+    .get('https://jsonplaceholder.typicode.com/photos')
+    .then(response => {
+      logger.info(`Received photos: ${JSON.stringify(response)}`);
+      return response;
+    })
+    .catch(() => {
+      throw errors.external_api_error('Error in external API');
+    });
