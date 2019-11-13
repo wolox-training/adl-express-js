1d8352e1c795f281c8590511d7899f86b0390d4d
alanlamaswolox <alan.lamas@wolox.com.ar>
2019-11-13 16:52:23 -0300

    created user model and migration

---
+++ migrations/migrations/20191113182159-create_users.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.createTable('users', {
+      id: { type: Sequelize.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true, unique: true },
+      first_name: { type: Sequelize.STRING, allowNull: false },
+      last_name: { type: Sequelize.STRING, allowNull: false },
+      email: { type: Sequelize.STRING, allowNull: false, unique: true },
+      password: { type: Sequelize.STRING, allowNull: false }
+    });
+  },
+
+  down: queryInterface => queryInterface.dropTable('users')
+};
