db9d5d9690afaf07a6e1ed05e7655864246f68a2
alanlamaswolox <alan.lamas@wolox.com.ar>
2019-11-13 11:57:36 -0300

    deleted logger in controllers

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -1,16 +1,15 @@
 const albumsService = require('../services/album');
-const logger = require('../logger');
 
 module.exports.albums = () => {
   albumsService
     .albums()
-    .then(albumsList => logger.info(albumsList))
+    .then(albumsList => albumsList)
     .catch('Error while displaying albums');
 };
 
 module.exports.photos = () => {
   albumsService
     .photos()
-    .then(photosList => logger.info(photosList))
+    .then(photosList => photosList)
     .catch('Error while displaying photos');
 };
