d6950d7f75215c956511d301fefc0dc60c31b7f4
alanlamaswolox <alan.lamas@wolox.com.ar>
2019-11-14 17:31:12 -0300

    changed users to user in table creation, added timestamps to it and underscored: true

--- migrations/migrations/20191113182159-create_users.js
+++ migrations/migrations/20191113182159-create_users.js
@@ -7,7 +7,9 @@ module.exports = {
       first_name: { type: Sequelize.STRING, allowNull: false },
       last_name: { type: Sequelize.STRING, allowNull: false },
       email: { type: Sequelize.STRING, allowNull: false, unique: true },
-      password: { type: Sequelize.STRING, allowNull: false }
+      password: { type: Sequelize.STRING, allowNull: false },
+      created_at: { allowNull: false, type: Sequelize.DATE },
+      updated_at: { allowNull: false, type: Sequelize.DATE }
     });
   },
 
