49332a1a7612c95d2e1b7ea628f4968faefafc6d
alanlamaswolox <alan.lamas@wolox.com.ar>
2019-11-15 18:07:30 -0300

    deleted innecesary code and added logic to check if user is creaed

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,17 +1,24 @@
-// eslint-disable-next-line no-empty-function
 const models = require('../models/index');
 
 module.exports.sign_up = (req, res) => {
-  models.users.create({
-    firstName: 'Roberto',
-    lastName: 'Gonzalez',
-    email: 'email@email.com',
-    password: 'pass'
-  });
+  const first_user = models.users
+    .findOne({ where: { email: 'email@email.com' } })
+    .then(user => user.dataValues)
+    .then(dataValues => {
+      console.log('--------------------------------------------------------------');
+      console.log(dataValues);
+      if (!dataValues) {
+        models.users.create({
+          firstName: 'Roberto',
+          lastName: 'Gonzalez',
+          email: 'email@email.com',
+          password: 'pass'
+        });
+      }
+    });
+
+  console.log(first_user);
   res.send({
     menssage: 'Esta ruta es de prueba en mi api restful con mongo y node'
   });
 };
-
-// ¿Por qué queda colgado el postman al hace esta petición?
-// ¿Cómo accedo al cuerpo de las peticiones GET o POST en los controladores?
