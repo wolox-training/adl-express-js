b94ea27c77f1346d7ef173af13667147834e4b74
alanlamaswolox <alan.lamas@wolox.com.ar>
2019-11-22 10:51:07 -0300

    added test to check response of sign up and if actually the user was created

---
+++ test/controllers/user.test.js
@@ -0,0 +1,36 @@
+const supertest = require('supertest');
+const models = require('../../app/models/index');
+const app = require('../../app');
+
+const request = supertest(app);
+
+describe('usersController.signUp', () => {
+  it('Creates a user', () => {
+    const userAttributes = {
+      firstName: 'Omar',
+      lastName: 'Rodriguez',
+      email: 'omar.rodriguez@wolox.com',
+      password: 'password1923'
+    };
+
+    return request
+      .post('/users')
+      .send(userAttributes)
+      .then(response => {
+        expect(response.text).toBe('Omar');
+      })
+      .then(() =>
+        models.user
+          .findOne({
+            where: {
+              firstName: 'Omar',
+              lastName: 'Rodriguez',
+              email: 'omar.rodriguez@wolox.com'
+            }
+          })
+          .then(user => {
+            expect(user.email).toBe('omar.rodriguez@wolox.com');
+          })
+      );
+  });
+});
