ff8f3d380f7a6648ca81cc1651aa8563c72b380a
alanlamaswolox <alan.lamas@wolox.com.ar>
2019-11-27 14:18:42 -0300

    added 'res' to middleware validate function

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,12 +1,13 @@
 const logger = require('../logger');
 const usersService = require('../services/users');
-// const signUpValidator = require('../middlewares/sign_up_validator');
 
-module.exports.signUp = (req, res, next) =>
-  usersService
+module.exports.signUp = (req, res, next) => {
+  console.log(req.body);
+  return usersService
     .signUp(req.body)
     .then(() => {
       logger.info(`Created user: ${JSON.stringify(req.body.firstName)}`);
       res.status(201).send({ firstName: req.body.firstName });
     })
     .catch(next);
+};
--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,7 @@
 const { healthCheck } = require('./controllers/healthCheck');
 const albumsController = require('./controllers/albums');
 const usersController = require('./controllers/users');
+const signUpMiddleware = require('./middlewares/sign_up_validator');
 
 exports.init = app => {
   app.get('/health', healthCheck);
@@ -11,5 +12,5 @@ exports.init = app => {
   // app.post('/endpoint/post/path', [], controller.methodPOST);
   app.get('/albums', albumsController.albums);
   app.get('/albums/:id/photos', albumsController.photos);
-  app.post('/users', usersController.signUp);
+  app.post('/users', [signUpMiddleware.validate], usersController.signUp);
 };
