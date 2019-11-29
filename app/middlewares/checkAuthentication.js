7aa4ee39f80c3c7abc57cc8738fe1249973be44f
alanlamaswolox <alan.lamas@wolox.com.ar>
2019-11-29 10:44:52 -0300

    fix auth middleware

--- app/middlewares/checkAuthentication.js
+++ app/middlewares/checkAuthentication.js
@@ -2,22 +2,24 @@ const jwt = require('jwt-simple');
 const errors = require('../errors');
 const models = require('../models/index');
 
+const decode = token => {
+  const result = jwt.decode(token, process.env.SECRET_KEY);
+  return models.user
+    .findOne({
+      where: { email: result }
+    })
+    .then(email => {
+      if (!email) {
+        throw errors.invalidCredentials();
+      }
+    });
+};
+
 module.exports.validate = (req, res, next) => {
   try {
-    const result = jwt.decode(req.headers.token, process.env.SECRET_KEY);
-    return models.user
-      .findOne({
-        where: { email: result }
-      })
-      .then(email => {
-        if (!email) {
-          throw errors.invalidCredentials();
-        }
-
-        return next();
-      });
-  } catch (error) {
-    res.status(401).send({ message: error.message });
+    decode(req.headers.token);
+  } catch (e) {
+    res.send({ error: e.message });
   }
 
   return next();
