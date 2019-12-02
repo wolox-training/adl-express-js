6285371427227b0598ea0604c886805b24da3791
alanlamaswolox <alan.lamas@wolox.com.ar>
2019-12-02 11:52:29 -0300

    refactored services

--- app/services/users.js
+++ app/services/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jwt-simple');
 const models = require('../models/index');
 const errors = require('../errors');
 const generatePassword = require('../helpers/users/generatePassword');
@@ -19,3 +21,16 @@ module.exports.signUp = body =>
         });
     }
   });
+
+module.exports.signIn = body =>
+  models.user
+    .findOne({
+      where: { email: body.email }
+    })
+    .then(user => {
+      if (!user || !bcrypt.compareSync(body.password, user.password)) {
+        throw errors.invalidCredentials('Invalid credentials, please try again');
+      }
+
+      return jwt.encode(user.email, process.env.SECRET_KEY);
+    });
