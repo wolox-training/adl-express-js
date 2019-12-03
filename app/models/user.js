55f8f475800457538d5fe305acf942819116e20a
alanlamaswolox <alan.lamas@wolox.com.ar>
2019-12-03 10:44:42 -0300

    updated user model and updated package.json with rollback command

--- app/models/user.js
+++ app/models/user.js
@@ -6,7 +6,12 @@ module.exports = (sequelize, DataTypes) => {
       firstName: { type: DataTypes.STRING, allowNull: false },
       lastName: { type: DataTypes.STRING, allowNull: false },
       email: { type: DataTypes.STRING, allowNull: false, unique: true },
-      password: { type: DataTypes.STRING, allowNull: false }
+      password: { type: DataTypes.STRING, allowNull: false },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'user'
+      }
     },
     {
       underscored: true
