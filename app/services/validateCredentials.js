2cdda207fc41310348d307a250a079c3414abe3d
alanlamaswolox <alan.lamas@wolox.com.ar>
2019-12-11 18:26:59 -0300

    updated users service

--- app/services/validateCredentials.js
+++ app/services/validateCredentials.js
@@ -10,12 +10,12 @@ module.exports.signIn = async body => {
   if (!user || !bcrypt.compareSync(body.password, user.password)) {
     throw errors.invalidCredentials('Invalid credentials, please try again');
   }
-  // const previousToken = await models.token.findOne({ where: { user_id: user.id } });
-  // if (previousToken) {
-  // await previousToken.destroy();
-  // }
+  const previousToken = await models.token.findOne({ where: { userId: user.id } });
+  if (previousToken) {
+    await previousToken.destroy();
+  }
 
-  // const newToken = await models.token.create({ user_id: user.id });
-  const tokenArray = { tokenId: 2, email: user.email };
+  const newToken = await models.token.create({ userId: user.id });
+  const tokenArray = { token: newToken.id, email: user.email };
   return jwt.encode(tokenArray, process.env.SECRET_KEY);
 };
